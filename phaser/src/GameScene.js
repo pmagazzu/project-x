f3d8033a5715b6777d21b1c8fd8e92209466ad83
Peter Magazzu <petermagazzu@peters-mac-mini.tail525fcd.ts.net>
2026-03-06 19:53:53 -0500

    Debug proof marker: END TURN (IGOUGO) + top bar IGOUGO LIVE r2 + console banner

--- phaser/src/GameScene.js
+++ phaser/src/GameScene.js
@@ -30,7 +30,7 @@ const SELECTED_STROKE  = 0xffe066;
 const HOVER_STROKE     = 0xaaddff;
 const MOVE_HIGHLIGHT   = 0x00ffcc;
 const ATTACK_HIGHLIGHT = 0xff6600;
-const BUILD_TAG = 'live-igougo-r1';
+const BUILD_TAG = 'IGOUGO LIVE r2';
 
 export class GameScene extends Phaser.Scene {
   constructor() { super('GameScene'); }
